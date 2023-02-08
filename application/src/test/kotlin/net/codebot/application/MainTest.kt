231e924cc9dde94ad8c2bb56a263dbe1c7f116f9
Yuying Li <anna.li.toronto@gmail.com>
2023-02-08 15:39:32 -0500

    add test and resources files

---
+++ application/src/test/kotlin/net/codebot/application/MainTest.kt
@@ -0,0 +1,5 @@
+package net.codebot.application
+
+import org.junit.jupiter.api.Assertions.*
+
+class MainTest
---
+++ console/src/test/kotlin/net/codebot/console/MainKtTest.kt
@@ -0,0 +1,5 @@
+package net.codebot.console
+
+import org.junit.jupiter.api.Assertions.*
+
+class MainKtTest
