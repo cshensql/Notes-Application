57e55e657db9761af1b85fa362282b5478454ad5
cshensql <cshencm@gmail.com>
2023-02-09 16:00:31 -0500

    minor change: not allowing nested folders

--- application/src/main/kotlin/business/Folder.kt
+++ application/src/main/kotlin/business/Folder.kt
@@ -2,5 +2,4 @@ package business
 
 class Folder(var name: String = "Untitled folder") : Pwd() {
     val noteList: MutableList<Note> = mutableListOf<Note>()
-    val folderList: MutableList<Folder> = mutableListOf<Folder>()
 }
