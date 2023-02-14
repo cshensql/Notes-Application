998c54aa85f3411ac50fa727e0642e32b9f5ec68
Charles Shen <c2999shen@uwaterloo.ca>
2023-02-14 11:56:48 -0500

    Resolve "Edit an existing note"

--- application/src/main/kotlin/presentation/FileListView.kt
+++ application/src/main/kotlin/presentation/FileListView.kt
@@ -1,4 +1,5 @@
 package presentation
+
 import business.Model
 
 import javafx.scene.control.TreeItem
@@ -15,6 +16,8 @@ class FileListView(model: Model) : IView, TreeView<String>() {
 
     private val root = TreeItem("Categories")
 
+    // list of dateCreated to help locate the correct note in noteList in model
+    private val dateCreatedList = mutableListOf<String>()
 
     init {
         val groupIcon = ImageView(Image("groupIcon.png", 18.0, 18.0, true, true))
@@ -30,15 +33,57 @@ class FileListView(model: Model) : IView, TreeView<String>() {
         root.children.addAll(groupRoot, noteRoot)
         this.setRoot(root)
         this.isFocusTraversable = false
+
+        this.setOnMouseClicked {
+            println(getNoteRootIndex())
+            val (isUnderNoteRoot, pos) = isUnderNoteRoot()
+            if (isUnderNoteRoot) {
+                val dateCreated = dateCreatedList[pos - 1]
+                model.updateSelection(dateCreated)
+            } else { // selection is not under "Notes"
+                // TODO
+            }
+        }
+    }
+
+    private fun getNoteRootIndex() = noteRoot.parent.children.indexOf(noteRoot) + 1
+
+    private fun isUnderNoteRoot(): Pair<Boolean, Int> {
+        val noteRootIndex = getNoteRootIndex()
+        val selectedIndex = this.selectionModel.selectedIndex
+        return Pair(selectedIndex > noteRootIndex, selectedIndex - noteRootIndex)
     }
 
     override fun updateView() {
+        val selectedIndex = this.selectionModel.selectedIndex
+        val numOfNotes = noteRoot.children.size
+
+        // remove all current content
         groupRoot.children.clear()
         noteRoot.children.clear()
+        dateCreatedList.clear()
 
         for (entry in model.noteList) {
             val noteItem = TreeItem(entry.value.title)
             noteRoot.children.add(noteItem)
+            dateCreatedList.add(entry.key)
+        }
+
+        val newNumOfNotes = noteRoot.children.size
+
+        // A note is added or deleted in the Notes section
+        if (newNumOfNotes != numOfNotes) {
+            val newIndex = dateCreatedList.indexOf(model.getCurrSelected().dateCreated)
+            if (newIndex < 0) {
+                // not able to find currSelected in Model,
+                // then select nothing
+                this.selectionModel.select(-1)
+            } else {
+                this.selectionModel.select(newIndex + 1 + getNoteRootIndex())
+            }
+        } else {
+            // TODO: Update selection if note is added or deleted in the Group area
+            this.selectionModel.select(selectedIndex)
         }
 
         this.refresh()
