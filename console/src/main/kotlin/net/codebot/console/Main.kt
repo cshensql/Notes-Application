bbf4acfa90b9aec9b2e100691f877caf0501fff7
Andy Yang <yz8yang@uwaterloo.ca>
2023-03-04 17:50:00 -0500

    remove excess

--- console/src/main/kotlin/net/codebot/console/Main.kt
+++ console/src/main/kotlin/net/codebot/console/Main.kt
@@ -6,7 +6,4 @@ fun main() {
     println("Console Application:")
     println("Hello ${SysInfo.userName}")
 
-    val runtime = Runtime.getRuntime()
-    val process = runtime.exec("cd ../../../resources && ./application")
-    println(process.inputStream.bufferedReader(Charsets.UTF_8).use { it.readText() })
 }
