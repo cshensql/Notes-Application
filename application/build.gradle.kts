6a96f38f85deaf260602167fba14a36247f57294
Yuying Li <anna.li.toronto@gmail.com>
2023-03-20 19:16:23 -0400

    add name

--- application/build.gradle.kts
+++ application/build.gradle.kts
@@ -50,6 +50,9 @@ javafx {
 // https://stackoverflow.com/questions/74453018/jlink-package-kotlin-in-both-merged-module-and-kotlin-stdlib
 jlink {
     forceMerge("kotlin")
+    launcher {
+        name = "Notes"
+    }
 }
 
 java {
