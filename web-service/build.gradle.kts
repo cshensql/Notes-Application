f9bfdc5a78ada75531788776f6c79b42e16900f3
Yuying Li <anna.li.toronto@gmail.com>
2023-04-05 16:33:56 -0400

    docker setup

--- web-service/build.gradle.kts
+++ web-service/build.gradle.kts
@@ -9,8 +9,7 @@ plugins {
 
 group = "com.cs346"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_11
-java.targetCompatibility = JavaVersion.VERSION_11
+java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
 	mavenCentral()
@@ -38,7 +37,7 @@ dependencyManagement {
 tasks.withType<KotlinCompile> {
 	kotlinOptions {
 		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "11"
+		jvmTarget = "17"
 	}
 }
 
